36904afdc991e5a2648b6079bad05a1886da8830
echin98 <ericchin1998@gmail.com>
2020-11-17 09:56:20 -0500

    merged in Hall code

--- main.c
+++ main.c
@@ -87,12 +87,37 @@ EPWM_SignalParams pwmSignal =
 void configurePhase(uint32_t base, uint32_t masterBase, uint16_t phaseVal);
 void switch_state_machine(enum switch_commands command);
 
+//
+// Interrupt Handler
+//
+__interrupt void gpioInterruptHandler(void);
 
 //
 // Main
 //
 void main(void)
 {
+    GPIO_setPinConfig(GPIO_32_GPIO32);
+    GPIO_setDirectionMode(HALLA_PIN,GPIO_DIR_MODE_IN);
+    GPIO_setPadConfig(HALLA_PIN, GPIO_PIN_TYPE_STD);
+    GPIO_setMasterCore(HALLA_PIN, GPIO_CORE_CPU1);
+    GPIO_setQualificationMode(HALLA_PIN, GPIO_QUAL_ASYNC);
+
+
+    GPIO_setPinConfig(GPIO_67_GPIO67);
+    GPIO_setDirectionMode(HALLB_PIN,GPIO_DIR_MODE_IN);
+    GPIO_setPadConfig(HALLB_PIN, GPIO_PIN_TYPE_STD);
+    GPIO_setMasterCore(HALLB_PIN, GPIO_CORE_CPU1);
+    GPIO_setQualificationMode(HALLB_PIN, GPIO_QUAL_ASYNC);
+
+
+
+    GPIO_setPinConfig(GPIO_111_GPIO111);
+    GPIO_setDirectionMode(HALLC_PIN,GPIO_DIR_MODE_IN);
+    GPIO_setPadConfig(HALLC_PIN, GPIO_PIN_TYPE_STD);
+    GPIO_setMasterCore(HALLC_PIN, GPIO_CORE_CPU1);
+    GPIO_setQualificationMode(HALLC_PIN, GPIO_QUAL_ASYNC);
+
     Device_init();
     Interrupt_initModule();
     Interrupt_initVectorTable();
@@ -100,6 +125,30 @@ void main(void)
     Interrupt_enableMaster();
 
     Board_init();
+
+    /* INTERRUPT SETUP */
+    GPIO_setInterruptType(GPIO_INT_XINT1, GPIO_INT_TYPE_BOTH_EDGES);
+    GPIO_setInterruptType(GPIO_INT_XINT2, GPIO_INT_TYPE_BOTH_EDGES);
+    GPIO_setInterruptType(GPIO_INT_XINT3, GPIO_INT_TYPE_BOTH_EDGES);
+
+    GPIO_setInterruptPin(HALLA_PIN, GPIO_INT_XINT1);
+    GPIO_setInterruptPin(HALLB_PIN, GPIO_INT_XINT2);
+    GPIO_setInterruptPin(HALLC_PIN, GPIO_INT_XINT3);
+
+    GPIO_enableInterrupt(GPIO_INT_XINT1);
+    GPIO_enableInterrupt(GPIO_INT_XINT2);
+    GPIO_enableInterrupt(GPIO_INT_XINT3);
+
+
+    Interrupt_register(INT_XINT1, &gpioInterruptHandler); //create interrupt handler that would sync with sensing
+    Interrupt_register(INT_XINT2, &gpioInterruptHandler);
+    Interrupt_register(INT_XINT3, &gpioInterruptHandler);
+
+    Interrupt_enable(INT_XINT1);
+    Interrupt_enable(INT_XINT2);
+    Interrupt_enable(INT_XINT3);
+
+    /* INTERRUPT SETUP */
 
     /* PWM SETUP */
 
@@ -149,11 +198,114 @@ void main(void)
 
     for(;;)
     {
-        switch_state_machine(INCREMENT);
-        DEVICE_DELAY_US(1000);
+        ;
+        //switch_state_machine(INCREMENT);
+        //DEVICE_DELAY_US(1000);
     }
 
 
+}
+
+__interrupt void gpioInterruptHandler(void)
+{
+    uint16_t pinValue1;
+    uint16_t pinValue2;
+    uint16_t pinValue3;
+
+    pinValue1 = GPIO_readPin(HALLA_PIN);
+    pinValue2 = GPIO_readPin(HALLB_PIN);
+    pinValue3 = GPIO_readPin(HALLC_PIN);
+
+    if (pinValue1 && pinValue2 && pinValue3==0) //case 0
+    {
+
+        //errors present when building for setPinConfig. When integrated with switching will go away
+                    GPIO_setPinConfig(HSA_GPIO);
+                    GPIO_setPinConfig(LSA_GPIO);
+                    GPIO_setPinConfig(HSB_GPIO);
+                    GPIO_setPinConfig(LSB_GPIO);
+                    GPIO_setPinConfig(HSC_GPIO);
+                    GPIO_setPinConfig(LSC_GPIO);
+
+                    GPIO_writePin(HSA_PIN,0);
+                    GPIO_writePin(LSA_PIN,0);
+                    GPIO_writePin(HSB_PIN,0);
+                    GPIO_writePin(LSB_PIN,0);
+                    GPIO_writePin(HSC_PIN,0);
+                    GPIO_writePin(LSC_PIN,0);
+
+    }
+
+    if (pinValue1==1 && pinValue2==0 && pinValue3==1) //case 1
+    {
+                    GPIO_setPinConfig(HSA_PWM); //copied from switching
+                    GPIO_setPinConfig(LSA_GPIO);
+                    GPIO_setPinConfig(HSB_GPIO);
+                    GPIO_setPinConfig(LSB_PWM);
+                    GPIO_setPinConfig(HSC_GPIO);
+                    GPIO_setPinConfig(LSC_GPIO);
+    }
+
+    if (pinValue1==1 && pinValue2==0 && pinValue3==0) //case 2
+       {
+
+                   GPIO_setPinConfig(HSA_PWM);
+                   GPIO_setPinConfig(LSA_GPIO);
+                   GPIO_setPinConfig(HSB_GPIO);
+                   GPIO_setPinConfig(LSB_GPIO);
+                   GPIO_setPinConfig(HSC_GPIO);
+                   GPIO_setPinConfig(LSC_PWM);
+
+       }
+
+    if (pinValue1==1 && pinValue2==1 && pinValue3==0) //case 3
+          {
+                    GPIO_setPinConfig(HSA_GPIO);
+                    GPIO_setPinConfig(LSA_GPIO);
+                    GPIO_setPinConfig(HSB_PWM);
+                    GPIO_setPinConfig(LSB_GPIO);
+                    GPIO_setPinConfig(HSC_GPIO);
+                    GPIO_setPinConfig(LSC_PWM);
+          }
+
+    if (pinValue1==0 && pinValue2==1 && pinValue3==0) //case 4
+          {
+
+
+                    GPIO_setPinConfig(HSA_GPIO);
+                    GPIO_setPinConfig(LSA_PWM);
+                    GPIO_setPinConfig(HSB_PWM);
+                    GPIO_setPinConfig(LSB_GPIO);
+                    GPIO_setPinConfig(HSC_GPIO);
+                    GPIO_setPinConfig(LSC_GPIO);
+
+          }
+    if (pinValue1==0 && pinValue2==1 && pinValue3==1) //case 5
+          {
+                    GPIO_setPinConfig(HSA_GPIO);
+                    GPIO_setPinConfig(LSA_PWM);
+                    GPIO_setPinConfig(HSB_GPIO);
+                    GPIO_setPinConfig(LSB_GPIO);
+                    GPIO_setPinConfig(HSC_PWM);
+                    GPIO_setPinConfig(LSC_GPIO);
+
+
+          }
+
+    if (pinValue1==0 && pinValue2==0 && pinValue3==1) //case 6
+              {
+
+                    GPIO_setPinConfig(HSA_GPIO);
+                    GPIO_setPinConfig(LSA_GPIO);
+                    GPIO_setPinConfig(HSB_GPIO);
+                    GPIO_setPinConfig(LSB_PWM);
+                    GPIO_setPinConfig(HSC_PWM);
+                    GPIO_setPinConfig(LSC_GPIO);
+
+                  }
+
+    Interrupt_clearACKGroup(INTERRUPT_ACK_GROUP1); //subject to change
+    Interrupt_clearACKGroup(INTERRUPT_ACK_GROUP2); //subject to change
 }
 
 //
