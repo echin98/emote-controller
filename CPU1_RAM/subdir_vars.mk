f2d1c19170f2ed75a22fe9a8c67dbb1d7918c1bb
echin98 <ericchin1998@gmail.com>
2020-11-21 09:06:03 -0500

    is this okay?

--- CPU1_RAM/subdir_vars.mk
+++ CPU1_RAM/subdir_vars.mk
@@ -12,26 +12,32 @@ LIB_SRCS += \
 C:/ti/C2000Ware_3_03_00_00_Software/driverlib/f2837xd/driverlib/ccs/Debug/driverlib.lib 
 
 C_SRCS += \
+../Trigger3ADCs.c \
 ../board.c \
 ../main.c 
 
 C_DEPS += \
+./Trigger3ADCs.d \
 ./board.d \
 ./main.d 
 
 OBJS += \
+./Trigger3ADCs.obj \
 ./board.obj \
 ./main.obj 
 
 OBJS__QUOTED += \
+"Trigger3ADCs.obj" \
 "board.obj" \
 "main.obj" 
 
 C_DEPS__QUOTED += \
+"Trigger3ADCs.d" \
 "board.d" \
 "main.d" 
 
 C_SRCS__QUOTED += \
+"../Trigger3ADCs.c" \
 "../board.c" \
 "../main.c" 
 
